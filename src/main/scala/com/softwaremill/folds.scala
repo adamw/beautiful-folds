5fdb6cc13a327a67bddc35b1fe9fef668071eca5
adamw <adam@warski.org>
2016-11-11 12:19:18 +0100

    More examples

--- src/main/scala/com/softwaremill/folds.scala
+++ src/main/scala/com/softwaremill/folds.scala
@@ -1,11 +1,9 @@
 package com.softwaremill
 
 import cats._
-import cats.data._
 import cats.implicits._
 import monocle.{Lens, Optional, Prism}
 
-import scala.collection.immutable.Seq
 import scala.util.Random
 
 trait Fold[I, O] {
@@ -38,8 +36,7 @@ object Fold {
 
   def sum2[M](implicit m: Monoid[M]): Fold[M, M] = Fold(m)(identity, identity)
 
-  /*
-  def combine[I, O1, O2](f1: Fold[I, O1], f2: Fold[I, O2]): Fold[I, (O1, O2)] = new Fold[I, (O1, O2)] {
+  def combine2[I, O1, O2](f1: Fold[I, O1], f2: Fold[I, O2]): Fold[I, (O1, O2)] = new Fold[I, (O1, O2)] {
     override type M = (f1.M, f2.M)
     override def m = new Monoid[M] {
       override def empty = (f1.m.empty, f2.m.empty)
@@ -48,7 +45,6 @@ object Fold {
     override def tally = i => (f1.tally(i), f2.tally(i))
     override def summarize = x => (f1.summarize(x._1), f2.summarize(x._2))
   }
-  */
 }
 
 object Example1 extends App {
@@ -60,7 +56,6 @@ object Example1 extends App {
 
 object Example2 extends App {
   import Fold._
-  import Fractional.Implicits._
 
   case class Average[A](numerator: A, denominator: Int)
 
@@ -71,20 +66,11 @@ object Example2 extends App {
       l.denominator+r.denominator)
   }
 
-  /*
-  def average[A: Fractional]: Fold[A, A] = new Fold[A, A] {
-    type M = Average[A]
-    val m = averageMonoid
-    def tally = Average[A](_, 1)
-    def summarize = a => a.numerator / implicitly[Fractional[A]].fromInt(a.denominator)
-  }
-  */
-
-  def average[A: Fractional]: Fold[A, A] = Fold(averageMonoid)(
+  def average[A: Numeric]: Fold[A, Double] = Fold(averageMonoid)(
     Average[A](_, 1),
-    a => a.numerator / implicitly[Fractional[A]].fromInt(a.denominator))
+    a => implicitly[Numeric[A]].toDouble(a.numerator) / a.denominator)
 
-  println(fold(1.0 to 10.0 by 1.0)(average))
+  println(fold(1 to 10)(average))
 }
 
 object Example3 extends App {
@@ -132,7 +118,7 @@ object Example5 extends App {
 
 object Example6 extends App {
   import Fold._
-  import Example3.product
+  import Example3.{product, length}
   import cats.syntax.cartesian._
 
   def foldFunctor[I] = new Functor[({type F[X]=Fold[I, X]})#F] {
@@ -147,28 +133,43 @@ object Example6 extends App {
     )
   }
 
+  {
+    import cats.syntax.functor._
+    sum[Int].map(s => "Sum is $s")
+  }
+
   def combine[I, O1, O2](f1: Fold[I, O1], f2: Fold[I, O2]): Fold[I, (O1, O2)] = foldApplicative.map2(f1, f2)((_, _))
 
-  println(fold(1 to 10)(combine(sum, product)))
-  println(fold(1 to 10)((sum[Int] |@| product).map((_, _))))
+  println(fold(1 to 10)(combine2(sum, length)))
+  println(fold(1 to 10)((sum[Int] |@| length |@| product).map((_, _, _))))
 }
 
 object Example7 extends App {
   import Fold._
+  import Example3.length
   import Example6._
+
+  def average[A: Numeric: Monoid]: Fold[A, Double] = (sum[A] |@| length[A]).map {
+    case (s, l) => implicitly[Numeric[A]].toDouble(s) / l
+  }
+
+  println(fold(1 to 10)(average))
+
+  //
+
   import Fractional.Implicits._
 
-  def length[A: Numeric: Monoid]: Fold[A, A] = Fold(implicitly[Monoid[A]])(_ => implicitly[Numeric[A]].one, identity)
+  def length2[A: Numeric: Monoid]: Fold[A, A] = Fold(implicitly[Monoid[A]])(_ => implicitly[Numeric[A]].one, identity)
+  def average2[A: Fractional: Monoid]: Fold[A, A] = (sum[A] |@| length2[A]).map(_ / _)
 
-  def average[A: Fractional: Monoid]: Fold[A, A] = (sum[A] |@| length[A]).map(_ / _)
-
-  println(fold(1.0 to 10.0 by 1.0)(average[Double]))
+  println(fold(1.0 to 10.0 by 1.0)(average2[Double]))
 }
 
 object Example8 extends App {
   import Fold._
+  import Example3.length
   import Example6._
-  import Example7._
+  import cats.syntax.functor._
 
   implicit def foldIsNumeric[A, B: Numeric]: Numeric[Fold[A, B]] = new Numeric[Fold[A, B]] {
     import Numeric.Implicits._
@@ -212,13 +213,11 @@ object Example8 extends App {
 
   {
     import Fractional.Implicits._
-
-    println(fold(1.0 to 10.0 by 1.0)(sum[Double] / length))
+    println(fold(1.0 to 10.0 by 1.0)(sum[Double] / length[Double].map(_.toDouble)))
   }
 }
 
 object Example9 extends App {
-  import Fold._
   import Example7._
 
   def foldNested[I, O](input: Seq[Seq[I]])(f: Fold[I, O]): O = {
@@ -231,11 +230,46 @@ object Example9 extends App {
     f.summarize(reduced)
   }
 
-  println(foldNested(List.fill(4)(1.0 to 100000.0 by 1.0))(average))
+  println(foldNested(List.fill(4)(1L to 100000L))(average))
 }
 
 object Example10 extends App {
+  import Fold._
+  import Example7._
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.{Await, Future}
 
+  def foldFutures[I, O](input: Seq[Future[I]])(f: Fold[I, O]): Future[O] = {
+    def doReduce(in: Seq[Future[I]], acc: f.M): Future[f.M] = in match {
+      case Seq(h, t@_*) => h.flatMap(hh => doReduce(t, f.m.combine(acc, f.tally(hh))))
+      case _ => Future.successful(acc)
+    }
+
+    val reduced = doReduce(input, f.m.empty)
+
+    reduced.map(f.summarize)
+  }
+
+  import scala.concurrent.duration._
+
+  val input: List[Future[Int]] = List(1, 2, 3).map(Future.successful)
+  println(Await.result(foldFutures(input)(sum), 1.minute))
+
+  val input2 = (1L to 1000000L).map(Future.successful)
+  println(Await.result(foldFutures(input2)(average), 1.minute))
+}
+
+object LensTutorial extends App {
+  case class Person(name: String, address: Address)
+  case class Address(street: String, country: String)
+
+  val streetLens = Lens { p: Person => p.address.street } { newStreet: String => p: Person =>
+    p.copy(address = p.address.copy(street = newStreet))
+  }
+
+  val aPerson = Person("Adam", Address("Functional Dr.", "Poland"))
+  println(streetLens.get(aPerson))
+  println(streetLens.set("OO Ln.")(aPerson))
 }
 
 object Example11 extends App {
@@ -256,6 +290,10 @@ object Example11 extends App {
   def _None[A]: Prism[Option[A], Unit] = Prism[Option[A], Unit](_.fold(Option(()))(_ => None))(_ => None)
   def _Left[A, B]: Prism[Either[A, B], A] = Prism[Either[A, B], A](_.fold(Some(_), _ => None))(Left(_))
   def _Right[A, B]: Prism[Either[A, B], B] = Prism[Either[A, B], B](_.fold(_ => None, Some(_)))(Right(_))
+
+  println(_Left.getOption(Left("x")))
+  println(_Left.getOption(Right(10)))
+
   println(fold(List(Some(1), None, Some(4)))(focus(_Some[Int], sum[Int])))
   println(fold(List(Some(1), None, Some(4)))(focus(_None[Int], length[Unit])))
   println(fold(List[Either[Int, String]](Left(1), Right("x"), Left(10), Left(5), Right("y")))(focus(_Left[Int, String], sum[Int])))
